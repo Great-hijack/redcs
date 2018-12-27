a6c3ed3636c0d57aadcb79c0d83a4d752c805028
mac <mac@Jade-MBP.local>
2018-12-27 10:57:31 +0700

    lang set for combine-search, case-view

---
+++ src/languages/combine-search.lang.ts
@@ -0,0 +1,7 @@
+export class CombineSearchLang{
+    TITLE = ['Combination Search', 'Tiềm kiếm'];
+    lbDoB = ['Date of Birth', 'Ngày sinh'];
+    lbAmpDate = ['Amputation Date', 'Ngày cắt cụt'];
+    lbSP = ['Service Provider', 'Service Provider'];
+    btnSearch = ['SEARCH', 'TÌM'];
+}
--- src/pages/combination-search/combination-search.ts
+++ src/pages/combination-search/combination-search.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CrudService } from '../../services/crud.service';
 
+import { CombineSearchLang } from '../../languages/combine-search.lang';
+import { LangService } from '../../services/lang.service';
 
 @IonicPage()
 @Component({
@@ -15,24 +17,41 @@ export class CombinationSearchPage {
     PAT_SP: ''
   }
 
-  SERVICEPROVIDERS = [ 'SP1', 'SP2', 'SP3']
+  SERVICEPROVIDERS = ['SP1', 'SP2', 'SP3'];
+  TITLE;
+  lbDoB;
+  lbAmpDate;
+  lbSP;
+  btnSearch;
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
-    private crudService: CrudService
-    ) {
+    private crudService: CrudService,
+    private langService: LangService
+  ) {
+    this.initLang();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CombinationSearchPage');
   }
 
-  searchCombination(){
+  initLang() {
+    let lang = new CombineSearchLang();
+    let i = this.langService.index;
+    this.TITLE = lang.TITLE[i];
+    this.lbAmpDate = lang.lbAmpDate[i];
+    this.lbDoB = lang.lbDoB[i];
+    this.lbSP = lang.lbSP[i];
+    this.btnSearch = lang.btnSearch[i];
+  }
+
+  searchCombination() {
     console.log(this.COMBINATION);
-    this.crudService.getPatientsWithCombination(this.COMBINATION.PAT_YoB,this.COMBINATION.PAT_AMP_DATE,this.COMBINATION.PAT_SP)
-    .then(res=>{
-      console.log(res);
-    })
+    this.crudService.getPatientsWithCombination(this.COMBINATION.PAT_YoB, this.COMBINATION.PAT_AMP_DATE, this.COMBINATION.PAT_SP)
+      .then(res => {
+        console.log(res);
+      })
   }
 
 }
