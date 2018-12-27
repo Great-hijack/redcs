1a2e53343277d23b21ee01590ba1af5758166dec
mac <mac@Jade-MBP.local>
2018-12-27 12:48:25 +0700

    language setting for privacy-update

---
+++ src/languages/privacy-update.lang.ts
@@ -0,0 +1,37 @@
+export class PrivacyUpdateLang {
+    TITLE = ['privacy-update', 'RIÊNG TƯ'];
+    headerBasicInfo = ['BASIC INFORMATION', 'THÔNG TIN'];
+    lbFName = ['First Name', 'Tên'];
+    lbLName = ['Last Name', 'Họ'];
+    lbDoB = ['Date of Birth', 'Ngày sinh'];
+    lbGender = ['Gender', 'Giới tính'];
+    lbTel = ['Tel', 'SĐT'];
+    headerMAILADDRESS = ['MAILING ADDRESS', 'ĐỊA CHỈ NHẬN THƯ'];
+    headerRESADDRESS = ['RESIDENCE ADDRESS', 'ĐỊA CHỈ HỘ KHẨU']
+    lbPROVINCE = ['PROVINCE', 'TỈNH/THÀNH'];
+    lbDISTRICT = ['DISTRICT', 'QUẬN/HUYỆN'];
+    lbWARD = ['WARD', 'PHƯỜNG/XÃ'];
+    lbAddress = ['Address', 'Địa chỉ'];
+    headerNON_AMPUTEE = ['NON AMPUTEE', 'NON AMPUTEE'];
+    lbYoDisability = ['Year of Disability', 'Năm bị tật'];
+    lbToDisability = ['Type of Disability', 'Loại Tật'];
+    lbDisabledParts = ['Disabled Parts', 'Phần khuyết tật'];
+    lbDisbaledReasons = ['Disabled Reasons', 'Lý do bị tật'];
+    lbReceivedSupportBefore = ['Received support before?', 'Đã nhận được hỗ trợ'];
+    lbWhatSupport = ['What support?', 'Loại hỗ trợ'];
+    lbYoReceivedSupport = ['Year of Received Support', 'Năm nhận hỗ trợ'];
+    lbYoLastSupport = ['Year of Last Support', 'Năm nhận hỗ trợ lần cuối'];
+    lbDisabledLastSponsors = ['Disabled Last Sponsors', 'Nhà tài trợ'];
+    headerAMPUTEE = ['AMPUTEE', 'AMPUTEE'];
+    lbAMPUTATIONDATE = ['AMPUTATION DATE', 'Năm bị cắt cụt'];
+    lbAMPUTATIONLEVELs = ['AMPUTATION LEVELs', 'Mức độ bị cắt cụt'];
+    lbAMPUTATIONCAUSE = ['AMPUTATION CAUSE', 'Nguyên nhận'];
+    lbHowManyAmputations = ['How many amputations', 'Bao nhiêu lần bị cắt cụt'];
+    lbLASTFITTINGDATE = ['LAST FITTING DATE', 'Lần thử cuối ngày nào'];
+    lbAMPUTATIONSPONSOR = ['AMPUTATION SPONSOR', 'Nhà tài trợ'];
+    headerOTHER = ['OTHER', 'KHÁC'];
+    lbCURRENTJOB = ['CURRENT JOB', 'VIỆC HIỆN TẠI'];
+    lbCreatedOn = ['Created on', 'Ngày tạo'];
+    btnCancel = ['Cancel', 'HUỶ'];
+    btnUpdate = ['Update', 'CẬP NHẬT'];
+}
--- src/pages/privacy-update/privacy-update.ts
+++ src/pages/privacy-update/privacy-update.ts
@@ -4,8 +4,8 @@ import { iPatient } from '../../interfaces/patient.interface';
 import { LocalService } from '../../services/local.service';
 import { CrudService } from '../../services/crud.service';
 import { LoadingService } from '../../services/loading.service';
-
-
+import { LangService } from '../../services/lang.service';
+import { PrivacyUpdateLang } from '../../languages/privacy-update.lang';
 @IonicPage()
 @Component({
   selector: 'page-privacy-update',
@@ -13,25 +13,65 @@ import { LoadingService } from '../../services/loading.service';
 })
 export class PrivacyUpdatePage {
   PRIVACY: any;
+
+  // LANGUAGESE SETTING
+  TITLE;
+  headerBasicInfo;
+  lbFName;
+  lbLName;
+  lbDoB;
+  lbGender;
+  lbTel;
+  headerMAILADDRESS;
+  headerRESADDRESS;
+  lbPROVINCE;
+  lbDISTRICT;
+  lbWARD;
+  lbAddress;
+  headerNON_AMPUTEE;
+  lbYoDisability;
+  lbToDisability;
+  lbDisabledParts;
+  lbDisbaledReasons;
+  lbReceivedSupportBefore;
+  lbWhatSupport;
+  lbYoReceivedSupport;
+  lbYoLastSupport;
+  lbDisabledLastSponsors;
+  headerAMPUTEE;
+  lbAMPUTATIONDATE;
+  lbAMPUTATIONLEVELs;
+  lbAMPUTATIONCAUSE;
+  lbHowManyAmputations;
+  lbLASTFITTINGDATE;
+  lbAMPUTATIONSPONSOR;
+  headerOTHER;
+  lbCURRENTJOB;
+  lbCreatedOn;
+  btnCancel;
+  btnUpdate;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private localService: LocalService,
     private crudService: CrudService,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private langService: LangService
   ) {
     this.PRIVACY = this.localService.PRIVACY_DEFAULT;
+    this.initLang();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PrivacyUpdatePage');
-    if(this.localService.BASIC_INFOS){
+    if (this.localService.BASIC_INFOS) {
       this.PRIVACY = this.localService.BASIC_INFOS.PRIVACY;
-    }else{
+    } else {
       this.crudService.getBasicData()
-      .then(()=>{
-        this.PRIVACY = this.localService.BASIC_INFOS.PRIVACY;
-      })
+        .then(() => {
+          this.PRIVACY = this.localService.BASIC_INFOS.PRIVACY;
+        })
     }
   }
 
@@ -53,21 +93,62 @@ export class PrivacyUpdatePage {
     }
   }
 
-  updateBasicData(DATA){
+  updateBasicData(DATA) {
     this.crudService.updateBasicData(DATA)
-    .then(res=>{
-      console.log(res);
-      this.loadingService.hideLoading();
-      this.navCtrl.pop();
-    })
-    .catch(err=>{
-      console.log(err);
-      this.loadingService.hideLoading();
-    })
+      .then(res => {
+        console.log(res);
+        this.loadingService.hideLoading();
+        this.navCtrl.pop();
+      })
+      .catch(err => {
+        console.log(err);
+        this.loadingService.hideLoading();
+      })
   }
 
-  cancel(){
+  cancel() {
     this.navCtrl.pop();
+  }
+
+  initLang() {
+    let lang = new PrivacyUpdateLang();
+    let i = this.langService.index;
+    this.TITLE = lang.TITLE[i];
+    this.headerBasicInfo = lang.headerBasicInfo[i];
+    this.lbFName = lang.lbFName[i];
+    this.lbLName = lang.lbLName[i];
+    this.lbDoB = lang.lbDoB[i];
+    this.lbGender = lang.lbGender[i];
+    this.lbTel = lang.lbTel[i];
+    this.headerMAILADDRESS = lang.headerMAILADDRESS[i];
+    this.headerRESADDRESS = lang.headerRESADDRESS[i];
+    this.lbPROVINCE = lang.lbPROVINCE[i];
+    this.lbDISTRICT = lang.lbDISTRICT[i];
+    this.lbWARD = lang.lbWARD[i];
+    this.lbAddress = lang.lbAddress[i];
+    this.headerNON_AMPUTEE = lang.headerNON_AMPUTEE[i];
+    this.lbYoDisability = lang.lbYoDisability[i];
+    this.lbToDisability = lang.lbToDisability[i];
+    this.lbDisabledParts = lang.lbDisabledParts[i];
+    this.lbDisbaledReasons = lang.lbDisbaledReasons[i];
+    this.lbReceivedSupportBefore = lang.lbReceivedSupportBefore[i];
+    this.lbWhatSupport = lang.lbWhatSupport[i];
+    this.lbYoReceivedSupport = lang.lbYoReceivedSupport[i];
+    this.lbYoLastSupport = lang.lbYoLastSupport[i];
+    this.lbDisabledLastSponsors = lang.lbDisabledLastSponsors[i];
+    this.headerAMPUTEE = lang.headerAMPUTEE[i];
+    this.lbAMPUTATIONDATE = lang.lbAMPUTATIONDATE[i];
+    this.lbAMPUTATIONLEVELs = lang.lbAMPUTATIONLEVELs[i];
+    this.lbAMPUTATIONCAUSE = lang.lbAMPUTATIONCAUSE[i];
+    this.lbHowManyAmputations = lang.lbHowManyAmputations[i];
+    this.lbLASTFITTINGDATE = lang.lbLASTFITTINGDATE[i];
+    this.lbAMPUTATIONSPONSOR = lang.lbAMPUTATIONSPONSOR[i];
+    this.headerOTHER = lang.headerOTHER[i];
+    this.lbCURRENTJOB = lang.lbCURRENTJOB[i];
+    this.lbCreatedOn = lang.lbCreatedOn[i];
+    this.btnCancel = lang.btnCancel[i];
+    this.btnUpdate = lang.btnUpdate[i];
+
   }
 
 }
